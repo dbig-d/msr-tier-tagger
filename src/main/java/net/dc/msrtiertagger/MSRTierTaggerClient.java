8d7ffac11c1f284f66243f8e1d003cb1e3e8d8e8
__bigd <netherite2022@gmail.com>
2026-04-27 09:28:15 +1000

    Initial commit

---
+++ src/main/java/net/dc/msrtiertagger/MSRTierTaggerClient.java
@@ -0,0 +1,10 @@
+package net.dc.msrtiertagger;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MSRTierTaggerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dc/msrtiertagger/MSRTierTaggerDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dc.msrtiertagger;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MSRTierTaggerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
